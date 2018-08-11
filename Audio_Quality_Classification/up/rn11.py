28c4f19e5004c0e47344ce783450730e25975f29
ravi jayanth <ravijayanth@ravis-MacBook-Pro.local>
2018-08-11 23:16:28 +0530

    succesful

--- Audio_Quality_Classification/up/rn11.py
+++ Audio_Quality_Classification/up/rn11.py
@@ -12,13 +12,12 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.svm import SVC
 from sklearn.neural_network import MLPClassifier
-import matplotlib.pyplot as plt
 from sklearn.learning_curve import learning_curve
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.metrics import confusion_matrix
 
 rows = []
-filename = 'output11.csv'
+filename = 'output_train.csv'
 with open(filename, 'r') as csvfile:
     csvreader = csv.reader(csvfile)
     fields = csvreader.next()
@@ -64,35 +63,6 @@ X3 = scaler.transform(rows1[:,0:137])
 #y1 = rows1[:,136]
 
 
-#Train decision tree model
-tree = DecisionTreeClassifier(random_state=0).fit(X_train, y_train)
-print("Decision Tree")
-print("Accuracy on training set: {:.3f}".format(tree.score(X_train, y_train)))
-print("Accuracy on test set: {:.3f}".format(tree.score(X_test, y_test)))
-print("Accuracy on test set: {:.3f}".format(tree.score(X1, y1)))
-
-#Train random forest model
-forest = RandomForestClassifier(n_estimators=5, random_state=0).fit(X_train, y_train)
-print("Random Forests")
-print("Accuracy on training set: {:.3f}".format(forest.score(X_train, y_train)))
-print("Accuracy on test set: {:.3f}".format(forest.score(X_test, y_test)))
-print("Accuracy on test set: {:.3f}".format(forest.score(X1, y1)))
-
-
-
-#Train support vector machine model
-
-
-def gaussianKernelGramMatrixFull(X1, X2, sigma=0.1):
-    """(Pre)calculates Gram Matrix K"""
-
-    gram_matrix = np.zeros((X1.shape[0], X2.shape[0]))
-    for i, x1 in enumerate(X1):
-        for j, x2 in enumerate(X2):
-            x1 = x1.flatten()
-            x2 = x2.flatten()
-            gram_matrix[i, j] = np.exp(- np.sum( np.power((x1 - x2),2) ) / float( 2*(sigma**2) ) )
-    return gram_matrix
 
 
 svm =SVC(C=2,gamma=0.03).fit(X_train, y_train) 
@@ -111,45 +81,3 @@ print(results1)
 print(results2)
 print(results3)
 #print("Accuracy on test set: {:.3f}".format(svm.score(X2, rows1[:,136])))
-
-#Train neural network model
-mlp = MLPClassifier(random_state=0,hidden_layer_sizes=(80,25)).fit(X_train, y_train)
-print("Multilayer Perceptron")
-print("Accuracy on training set: {:.3f}".format(mlp.score(X_train, y_train)))
-print("Accuracy on test set: {:.3f}".format(mlp.score(X_test, y_test)))
-print("Accuracy on test set: {:.3f}".format(mlp.score(X1, y1)))
-#print("Accuracy on test set: {:.3f}".format(mlp.score(X2, rows1[:,136])))
-#print(mlp.predict(X1))
-
-
-
-#train_sizes, train_scores, test_scores = learning_curve(
-        #svm, X_train, y_train, cv=10, n_jobs=-1, train_sizes=np.linspace(.1, 1., 10), verbose=0)
-
-#train_scores_mean = np.mean(train_scores, axis=1)
-#train_scores_std = np.std(train_scores, axis=1)
-#test_scores_mean = np.mean(test_scores, axis=1)
-#test_scores_std = np.std(test_scores, axis=1)
-
-#plt.figure()
-#plt.title("Classifier")
-#plt.legend(loc="best")
-#plt.xlabel("Training examples")
-#plt.ylabel("Score")
-#plt.ylim((0.2, 1.01))
-#plt.gca().invert_yaxis()
-#plt.grid()
-
-# Plot the average training and test score lines at each training set size
-#plt.plot(train_sizes, train_scores_mean, 'o-', color="b", label="Training score")
-#plt.plot(train_sizes, test_scores_mean, 'o-', color="r", label="Test score")
-
-# Plot the std deviation as a transparent range at each training set size
-#plt.fill_between(train_sizes, train_scores_mean - train_scores_std, train_scores_mean + train_scores_std, alpha=0.1, color="b")
-#plt.fill_between(train_sizes, test_scores_mean - test_scores_std, test_scores_mean + test_scores_std, alpha=0.1, color="r")
-
-# Draw the plot and reset the y-axis
-#plt.draw()
-#plt.show()
-#plt.gca().invert_yaxis()
-
